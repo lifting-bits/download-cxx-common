16760eaed984855d725b59ebbe69dc7924fb83bf
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-04-20 18:08:16 +0200

    action: print version

--- src/download-cxx-common.ts
+++ src/download-cxx-common.ts
@@ -2,7 +2,8 @@ import * as core from '@actions/core'
 
 async function run() {
     const llvm = core.getInput('llvm')
-    core.info(`Downloading cxx-common with llvm-${llvm}`)
+    const version = core.getInput('version')
+    core.info(`Downloading cxx-common-${version} with llvm-${llvm}`)
 }
 
 // Our main method: call the run() function and report any errors
