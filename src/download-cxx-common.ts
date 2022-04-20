8158dcf5799aef9b8f66d68c8cea6cefd5e27209
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-04-20 14:53:19 +0200

    src: apply format

--- src/download-cxx-common.ts
+++ src/download-cxx-common.ts
@@ -1,11 +1,9 @@
 import * as core from '@actions/core'
 
 async function run() {
-  const llvm = core.getInput("llvm");
-  core.info(`Downloading cxx-common with llvm-${llvm}`);
+    const llvm = core.getInput('llvm')
+    core.info(`Downloading cxx-common with llvm-${llvm}`)
 }
 
 // Our main method: call the run() function and report any errors
-run()
-  .catch(error => core.setFailed("Workflow failed! " + error.message));
-
+run().catch(error => core.setFailed('Workflow failed! ' + error.message))
