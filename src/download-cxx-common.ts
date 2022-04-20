044d7e4b4fe22daad78bb9e8de526267702f6ef5
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-04-20 14:39:40 +0200

    src: print llvm info

--- src/download-cxx-common.ts
+++ src/download-cxx-common.ts
@@ -1,7 +1,8 @@
 import * as core from '@actions/core'
 
 async function run() {
-
+  const llvm = core.getInput("llvm");
+  core.info(`Downloading cxx-common with llvm-${llvm}`);
 }
 
 // Our main method: call the run() function and report any errors
